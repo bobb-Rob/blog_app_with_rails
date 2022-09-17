56098a3cb372ab1c703ac50431e4520faf7b95d1
Robertson Arthur <robertsonakpan@gmail.com>
2022-09-17 15:50:57 +0100

    Validate Likes_counter variable in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,11 +7,14 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true }
   validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }
 
+  validates :likes_counter, numericality: { only_integer: true }
+  validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }
+
   def update_posts_counter
     author.increment!(:posts_counter)
   end
 
   def five_recent_comments
-    comments.order('created_at desc').limits(5)
+    comments.last(5).reverse
   end
 end
