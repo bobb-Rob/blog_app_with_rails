99f501a0aba0985c9b38698ab992117585941811
Robertson Arthur <robertsonakpan@gmail.com>
2022-09-18 22:26:38 +0100

    Add router file with appropriate routes definition

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # resources :users
+  root 'users#index'
+  get "/users/:id", to: "users#show"
+  get "/users/:id/posts", to: "posts#index"
+  get "/users/:id/posts/:id", to: "posts#show"
   # Defines the root path route ("/")
   # root "articles#index"
 end
