bbcee7e930030326fbfcb8a73f64fc9442ae8197
Robertson Arthur <robertsonakpan@gmail.com>
2022-09-19 11:02:23 +0100

    Redefine routes accurately using the helper method resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # resources :users
-  root 'users#index'
-  get '/users', to: 'users#index'
-  get "/users/:id", to: "users#show"
-  get "/users/:id/posts", to: "posts#index"
-  get "/users/:id/posts/:id", to: "posts#show"
+  root 'users#index' 
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+  # get "/users/:id/posts/:id", to: "posts#show"
   # Defines the root path route ("/")
   # root "articles#index"
 end
