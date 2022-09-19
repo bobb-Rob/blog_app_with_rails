4129d1c342ac75d7b742165fbe93a726f892de66
Robertson Arthur <robertsonakpan@gmail.com>
2022-09-19 10:08:51 +0100

    Add users routes to router.rb file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all unless Post.all.empty?
   end
 
   def show
-    @post_by_id = Post.find_by(id: params['id'])
+    @post_by_id = Post.find_by(id: params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # resources :users
   root 'users#index'
+  get '/users', to: 'users#index'
   get "/users/:id", to: "users#show"
   get "/users/:id/posts", to: "posts#index"
   get "/users/:id/posts/:id", to: "posts#show"
