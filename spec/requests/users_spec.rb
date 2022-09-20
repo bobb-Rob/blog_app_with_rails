c2f206896c6f24370ebc90822869618be62d1e70
Robertson Arthur <robertsonakpan@gmail.com>
2022-09-20 14:31:56 +0100

    Add test for index view on users_spec file

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:each) do
+      get "/users"
+    end
+
+    it 'Users index is successful' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'Renders correct template' do
+      expect(response).to render_template(:index)
+    end
   end
 end
