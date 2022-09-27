639f766e289ec8e464db0fd90324aa4a5efcd5b4
Robertson Arthur <robertsonakpan@gmail.com>
2022-09-27 14:49:34 +0100

    Add test to test for shows the comment each comment author left

--- spec/post_show_spec.rb
+++ spec/post_show_spec.rb
@@ -38,4 +38,10 @@ RSpec.describe "PostShow", type: :feature do
     expect(page).to have_content(comment.author.name)
   end
 
+  it 'shows the comment each comment author left' do    
+    comment = @post.comments.first
+    comment2 = @post.comments.last
+    expect(page).to have_content(comment.text)
+    expect(page).to have_content(comment2.text)
+  end
 end
